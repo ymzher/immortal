a2872bb57f7ced41bc147f6ca7a13449fd6b997f
ymzh <2368200705@qq.com>
2021-04-21 18:14:42 +0800

    基本逻辑

---
+++ immortal/Assets/Scripts/Basic/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T __inst = null;
+    public static T Instance
+    {
+        get
+        {
+            if (__inst == null)
+            {
+                __inst = GameObject.FindObjectOfType<T>();
+            }
+            return __inst;
+        }
+    }
+
+    private void Awake()
+    {
+        if (__inst == null)
+        {
+            __inst = this as T;
+        }
+    }
+    protected virtual void OnDestroy()
+    {
+        if (__inst == this)
+        {
+            __inst = null;
+        }
+    }
+}
---
+++ immortal/Assets/Scripts/Immortal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immortal : Singleton<Immortal>
+{
+    private float m_energy = 0;
+    private float m_health = 0.8f;
+    private float m_mood = 0.8f;
+
+    //private List<>丹药
+    private float m_cultivatingAppend = 1;
+    private float m_aptitude = 1;//改为灵根类型
+
+    private bool m_cultivatingImmortality = false;
+
+    void Start()
+    {
+        StartCultivateImmortality();
+    }
+
+    public bool StartCultivateImmortality()
+    {
+        m_cultivatingImmortality = true;
+        return true;
+    }
+    public bool StopCultivateImmortality()
+    {
+        m_cultivatingImmortality = false;
+        return true;
+    }
+    private void CultivatingImmortality()
+    {
+        //瓶颈判断
+
+        //环境加成*健康加成*心境加成*丹药加成*物品加成*功法加成*额外加成 * 个人基本单位（灵根）
+        m_energy += 1 * m_health * m_mood * 1 * 1 * 1 * m_cultivatingAppend * m_aptitude * Time.fixedDeltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_cultivatingImmortality)
+        {
+            CultivatingImmortality();
+        }
+        Debug.Log(m_energy);
+    }
+}
